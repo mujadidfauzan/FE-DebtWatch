0967d2677c072d1dc6d9b8ef756f6fff380c63b5
wandadesi <wandadesirahmawati@gmail.com>
2025-05-05 14:56:38 +0700

    ganti warna

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,30 +8,30 @@ const LoginPage: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-600 text-white p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-700 text-white p-4">
       <div className="w-full max-w-xs flex flex-col items-center">
         <h1 className="text-4xl font-bold mb-12">Welcome</h1>
 
         <div className="w-full space-y-4 mb-6">
           <div className="space-y-1 text-left">
-            <Label htmlFor="email" className="text-xs font-light text-gray-300">Username Or Email</Label>
+            <Label htmlFor="email" className="text-xs font-medium text-white">Username Or Email</Label>
             <Input 
               id="email" 
               type="email" 
               placeholder="example@example.com" 
-              className="bg-blue-600 border-b border-gray-400 rounded-none placeholder-gray-400 focus:border-white focus:ring-0" 
+              className="bg-white-600 border-b border-white-400 rounded-none placeholder-gray-300 !placeholder-gray-300 focus:border-white focus:ring-0"  
             />
           </div>
           <div className="space-y-1 text-left relative">
-            <Label htmlFor="password" className="text-xs font-light text-gray-300">Password</Label>
+            <Label htmlFor="password" className="text-xs font-medium text-white">Password</Label>
             <Input 
               id="password" 
               type="password" 
               placeholder="••••••••" 
-              className="bg-blue-600 border-b border-gray-400 rounded-none placeholder-gray-400 focus:border-white focus:ring-0" 
+              className="bg-white-600 border-b border-white-400 rounded-none placeholder-gray-300  !placeholder-gray-300 focus:border-white focus:ring-0" 
             />
           
-            <span className="absolute right-2 top-6 text-gray-400 cursor-pointer">👁️</span> 
+            <span className="absolute right-2 top-8 text-gray-400 cursor-pointer">👁️</span> 
           </div>
         </div>
 
@@ -48,7 +48,7 @@ const LoginPage: React.FC = () => {
         </div>
 
         <div className="text-center">
-          <p className="text-xs text-gray-300">
+          <p className="text-xs text-gray-200">
             Don't have an account? 
             <Link to="/signup" className="font-semibold text-white hover:underline ml-1">
               Sign Up
